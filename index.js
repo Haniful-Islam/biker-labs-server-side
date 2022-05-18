171f754e617c4907328c629d9115d26e7a8957cf
haniful-islam <haniful360@gmail.com>
2022-05-18 09:56:45 +0600

    signle bike added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express();
@@ -23,7 +23,14 @@ async function run() {
             const cursor = bikeCollection.find(query);
             const bikes = await cursor.toArray();
             res.send(bikes);
-        })
+        });
+
+        app.get('/bike/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const bike = await bikeCollection.findOne(query);
+            res.send(bike);
+        });
     }
     finally {
 
