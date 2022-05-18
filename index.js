d200a1d0ce6d1b40207898518f377a97b1a39a92
haniful-islam <haniful360@gmail.com>
2022-05-18 16:46:16 +0600

    manage item added

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
             const result = await bikeCollection.insertOne(newBike);
             res.send(result);
         });
+
+        //Delete 
+        app.delete('/bike/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bikeCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
