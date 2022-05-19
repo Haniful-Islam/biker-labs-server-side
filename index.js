3591b31b9c4c5c348f249fe622cbe70d3e022dcc
haniful-islam <haniful360@gmail.com>
2022-05-19 10:46:17 +0600

    all clear

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express();
 
-//middleware
+//Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -39,7 +39,7 @@ async function run() {
             res.send(result);
         });
 
-        //Delete 
+        //DELETE 
         app.delete('/bike/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
